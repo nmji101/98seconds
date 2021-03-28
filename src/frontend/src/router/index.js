8244c8daba1c55fa3609fdb4b088c32a0518e9b1
hyyun <nmji101@naver.com>
2021-03-28 12:30:13 +0900

    main 추가

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Main from "../views/Main";
+import ItemList from "../components/item/ItemList";
 
 Vue.use(Router)
 
@@ -8,8 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
+    },
+    {
+      path: '/ItemList',
+      name: 'ItemList',
+      component: ItemList
     }
   ]
 })
